ad87e3e032ceb57b1a729e6441dfb6192b5cfea3
Adam Kiryk <akiryk@wayfair.com>
2019-02-17 08:39:43 -0500

    Make function work

--- src/lambda/get-post.js
+++ src/lambda/get-post.js
@@ -1,26 +1,24 @@
 import faunadb from "faunadb";
-// const client = new faunadb.Client({
-//   secret: "fnADHZCWpsACB1sxl5UgB2Vk_W-G3Mi9fbWqBgI9"
-// });
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: "fnADHZCWpsACB1sxl5UgB2Vk_W-G3Mi9fbWqBgI9"
+});
 
 exports.handler = (event, context, callback) => {
-  console.log("Success");
-  // const id = getId(event.path);
-  // console.log(`Function 'todo-read' invoked. Read id: ${id}`);
-  // return client
-  //   .query(q.Get(q.Ref(q.Class("posts"), "224493754100220423")))
-  //   .then(response => {
-  //     console.log("success", response);
-  //     return callback(null, {
-  //       statusCode: 200,
-  //       body: JSON.stringify(response)
-  //     });
-  //   })
-  //   .catch(error => {
-  //     console.log("error", error);
-  //     return callback(null, {
-  //       statusCode: 400,
-  //       body: JSON.stringify(error)
-  //     });
-  //   });
+  return client
+    .query(q.Get(q.Ref(q.Class("posts"), "224493754100220423")))
+    .then(response => {
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      });
+    })
+    .catch(error => {
+      console.log("error", error);
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      });
+    });
 };
